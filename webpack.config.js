5cb752be32961610b9afd252e43eba5e51fad48c
cyber-eternal <92edgarsargsyan@gmail.com>
2019-08-27 09:23:50 +0400

    changes in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,56 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+// const HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: {
-    server: './server.js',
+    main: './server.js'
   },
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js',
-    pathinfo: true
+    filename: '[name].js'
   },
-  watch: true,
-  mode: process.env.STAGE || 'development',
-  target: 'node',
-  node: {
-    // Need this when working with express, otherwise the build fails
-    __dirname: false, // if you don't put this is, __dirname
-    __filename: false, // and __filename return blank or /
-  },
-  externals: [nodeExternals()], // Need this to avoid error when working with Express
+  target: 'web',
+  // devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        loader: 'babel-loader',
       },
       {
+        // Loads the javacript into html template provided.
+        // Entry point is set below in HtmlWebPackPlugin in Plugins 
         test: /\.html$/,
-        use: [{ loader: 'html-loader' }]
+        use: [
+          {
+            loader: 'html-loader',
+            //options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
       }
     ]
   },
-  stats: {
-    // copied from `'minimal'`
-    errors: true,
-    warnings: true,
-    // our additional options
-    moduleTrace: true,
-    errorDetails: true
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    module: 'empty',
+    child_process: 'empty'
   }
+  // plugins: [
+  //   new HtmlWebPackPlugin({
+  //     template: './index.html',
+  //     filename: './index.html',
+  //     excludeChunks: ['server']
+  //   })
+  // ]
 };
