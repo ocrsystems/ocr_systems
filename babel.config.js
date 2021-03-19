0b7600132ed2a3236295b583ef54cd4f3ddd9f8f
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2021-03-20 00:02:53 +0400

    feat: commitlint integration

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['@babel/env'],
-  plugins: [
-    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-proposal-private-methods',
-    '@babel/plugin-proposal-class-properties',
-  ]
-};
--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 import uuid from 'uuid';
-// import { spawn } from 'child_process';
 import upload from '../middleware/uploadMiddleware';
 import { getTextFromImage } from './../utils';
 
@@ -10,19 +9,17 @@ const router = express.Router();
 
 router.post('/', upload.single('image'), async (req, res) => {
   try {
-    const imagePath = path.join(`${__dirname}/../`, 'tmp');
-    const fileName = `${uuid.v1()}.png`;
-    const txtFileName = `${uuid.v1()}.txt`;
-    const filePath = path.resolve(`${imagePath}/${fileName}`);
+    const imagePath: string = path.join(`${__dirname}/../`, 'tmp');
+    const uniqueId: string = uuid.v1();
+    const fileName: string = `${uniqueId}.png`;
+    const txtFileName: string = `${uniqueId}.txt`;
+    const filePath: string = path.resolve(`${imagePath}/${fileName}`);
 
     if (!req.body.file) throw new Error('Please provide an image');
 
     const buf = Buffer.from(req.body.file.buffer.replace(/^data:image\/\w+;base64,/, ''), 'base64');
 
-    fs.writeFile(filePath, buf, 'binary', (err) => {
-      if (err) throw new Error('Error in image uploading');
-      return true;
-    });
+    fs.writeFileSync(filePath, buf, 'binary');
 
     const config = {
       lang: req.body.language,
@@ -30,10 +27,7 @@ router.post('/', upload.single('image'), async (req, res) => {
 
     const text = await getTextFromImage(config, `${imagePath}/${fileName}`);
 
-    fs.writeFile(`${imagePath}/${txtFileName}`, Buffer.from(text as string), 'binary', (err) => {
-      if (err) throw new Error('Error in txt saving');
-      return true;
-    });
+    fs.writeFileSync(`${imagePath}/${txtFileName}`, Buffer.from(text as string), 'binary');
 
     return res.status(200).send(text);
   } catch (error) {
