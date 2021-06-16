d646ddf99525172cccfd4e4a8b96a6b207792a89
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2021-06-16 14:39:03 +0400

    chore: testing

--- src/routes/test/index.ts
+++ src/routes/test/index.ts
@@ -5,11 +5,13 @@ const router = express.Router();
 router.get('/', (req, res) => {
   console.log('Test get request ###');
   console.log('Test get req###', req);
+  res.send(true);
 });
 
 router.post('/', (req, res) => {
   console.log('Test post request ###');
   console.log('Test post req###', req.body);
+  res.send(true);
 });
 
 export default router;
